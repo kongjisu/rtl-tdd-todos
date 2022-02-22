6392f3b4250219b4f65d2c073a28f47bbda8362d
jisu <kosu2389@naver.com>
2022-02-22 02:27:36 +0000

    todoform input test ok

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
+
 const TodoForm =() => {
+    const [value, setValue] = useState('');
+    const onChange = useCallback(e =>{
+        setValue(e.target.value);
+
+
+    }, []);
     return (
         <from>
-            <input placeholder ="할 일을 입력하시오"/>
+            <input placeholder ="할 일을 입력하시오" value={value} onChange={onChange} />
             <button type="submit">등록</button>
 
         </from>
--- src/TodoForm.test.js
+++ src/TodoForm.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react"; //react-testing-library; 이건 예전 정보임 
+import { fireEvent, render } from "@testing-library/react"; //react-testing-library; 이건 예전 정보임 
 import TodoForm from "./TodoForm";
 
 describe('<TodoForm/>', () =>{
@@ -9,6 +9,16 @@ describe('<TodoForm/>', () =>{
         getByPlaceholderText('할 일을 입력하시오'); //input 있는지 없는지
         getByText('등록');// button이 있는지 없는지 
 
+
+    })
+
+    it('change input',() =>{
+        const {getByPlaceholderText} =render(<TodoForm />);
+        const input =getByPlaceholderText('할 일을 입력하시오');
+        fireEvent.change(input, {target: {value: 'TDD 배우기'}});
+        expect(input).toHaveAttribute('value', 'TDD 배우기');
+        
+        
 
     })
 })
