612bb10734a85906ad25f7dfa4139534aeebf329
jisu <kosu2389@naver.com>
2022-02-22 02:12:37 +0000

    TodoForm ui test ok

---
+++ src/TodoForm.js
@@ -0,0 +1,15 @@
+import React from "react";
+const TodoForm =() => {
+    return (
+        <from>
+            <input placeholder ="할 일을 입력하시오"/>
+            <button type="submit">등록</button>
+
+        </from>
+
+    );
+
+
+}
+
+export default TodoForm;
