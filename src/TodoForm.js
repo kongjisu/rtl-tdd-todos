6847d627d6d2072f8a87f48f4a32279a1a178f73
jisu <kosu2389@naver.com>
2022-02-22 02:46:12 +0000

    todoform submit event test ok

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,21 +1,29 @@
 import React, { useCallback, useState } from "react";
 
-const TodoForm =() => {
+const TodoForm =({onInsert}) => {
     const [value, setValue] = useState('');
     const onChange = useCallback(e =>{
         setValue(e.target.value);
 
 
     }, []);
+    const onSubmit = useCallback(e => {
+        onInsert(value);
+        setValue('');
+        e.preventDefault();
+
+    }, [onInsert, value]);
+    
     return (
-        <from>
+        <from onSubmit={onSubmit}>
+            
             <input placeholder ="할 일을 입력하시오" value={value} onChange={onChange} />
             <button type="submit">등록</button>
 
         </from>
 
     );
-
+    
 
 }
 
--- src/TodoForm.test.js
+++ src/TodoForm.test.js
@@ -18,7 +18,17 @@ describe('<TodoForm/>', () =>{
         fireEvent.change(input, {target: {value: 'TDD 배우기'}});
         expect(input).toHaveAttribute('value', 'TDD 배우기');
         
-        
+        })
+        it('calls onInsert and clears input', () =>{
+            const onInsert = jest.fn();//브라우저에서의 이벤트를 사용할 수 있게 하는 것이다.  
+            const {getByText, getByPlaceholderText} = render(<TodoForm onInsert={onInsert} />);
+            const input = getByPlaceholderText('할 일을 입력하시오');
+            const button = getByText('등록');
+            fireEvent.change(input, {target: {value: 'tdd 배우기'}});
+            fireEvent.click(button);
+            expect(onInsert).toBeCalledWith('tdd 배우기');
+            expect(input).toHaveAttribute('value', '');
 
     })
+            
 })
